7bcd8dfb3386caa9579107b3d15e422bbc9fbc96
aqtagon <ahmed.danawi@gmail.com>
2024-01-24 22:40:13 -0800

    Added update user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -31,3 +31,24 @@ const userController = {
             res.status(400).json(err);
           });
       },
+      createUser({ body }, res) {
+        User.create(body)
+          .then((dbUserData) => res.json(dbUserData))
+          .catch((err) => res.status(400).json(err));
+      },
+      updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, body, {
+          new: true,
+          runValidators: true,
+        })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      },
