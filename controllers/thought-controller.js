6784a821a0d55a9f1dd03893193d14f10a2842a5
aqtagon <ahmed.danawi@gmail.com>
2024-01-24 22:09:37 -0800

    Added get thought by id and create new thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -12,3 +12,41 @@ const thoughtController = {
           res.status(400).json(err);
         });
     },
+
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.thoughtId })
+          .populate({
+            path: "thoughts",
+          })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No thought found by that id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      },
+
+      addThought({ params, body }, res) {
+        Thought.create(body)
+          .then(({ _id }) => {
+            console.log(_id);
+            return User.findOneAndUpdate(
+              { _id: body.userId },
+              { $push: { thoughts: _id } },
+              { new: true }
+            );
+          })
+          .then((dbUserData) => {
+            console.log(dbUserData);
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id!" });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch((err) => res.json(err));
+      },
