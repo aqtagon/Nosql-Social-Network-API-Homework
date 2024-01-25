8d4a5d8eaa63fc877180c5b0cd707f541e9fdd0e
aqtagon <ahmed.danawi@gmail.com>
2024-01-25 00:41:59 -0800

    Updated json

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ mongoose.connect('mongodb://localhost/social-network', {
 
 mongoose.set("debug", true);
 
-app.use(require('./routes'));
+app.use(require('./routes'));   
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
