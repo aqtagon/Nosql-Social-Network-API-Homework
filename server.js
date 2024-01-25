d63ea788d783bdac8894da8422cd96a0cd19ffec
aqtagon <ahmed.danawi@gmail.com>
2024-01-24 23:50:35 -0800

    Made changes for app to work

--- server.js
+++ server.js
@@ -8,11 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/social-network-api", {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+mongoose.connect('mongodb://localhost/social-network', {
 });
 
 mongoose.set("debug", true);
